337685810d55addb62026e4e187436bd34647afb
peperodri1 <jrrueda6@hotmail.com>
2025-09-29 10:11:34 +0200

    ultima version

---
+++ 5.ps1
@@ -0,0 +1,5 @@
+############################################
+########## ENTRADA- SALIDA  ################
+############################################
+$a=read-host -prompt "Introduce tu nombre"
+write-host "Hola " $a " Bienvenido al curso "
---
+++ sincroniza.ps1
@@ -0,0 +1,4 @@
+$m = read-host -prompt "Introduce mensaje"
+git add .
+git commit -m $m
+git push --force origin main
